e016e6a970a5c5b815bfb05a6ae77e4aaad189df
shamspias <shamsuddinpias0@gmail.com>
2022-10-16 17:41:28 +0600

    update celery and add task with user models and email

--- users/models.py
+++ users/models.py
@@ -1,3 +1,48 @@
+import uuid
 from django.db import models
+from django.dispatch import receiver
+from django.contrib.auth.models import AbstractUser
+from rest_framework_simplejwt.tokens import RefreshToken
+from easy_thumbnails.fields import ThumbnailerImageField
+from django.urls import reverse
+from django_rest_passwordreset.signals import reset_password_token_created
+from easy_thumbnails.signals import saved_file
+from easy_thumbnails.signal_handlers import generate_aliases_global
 
-# Create your models here.
+from common.helpers import build_absolute_uri
+from notifications.services import notify, ACTIVITY_USER_RESETS_PASS
+
+
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+    """
+    Handles password reset tokens
+    When a token is created, an e-mail needs to be sent to the user
+    """
+    reset_password_path = reverse('password_reset:reset-password-confirm')
+    context = {
+        'username': reset_password_token.user.username,
+        'email': reset_password_token.user.email,
+        'reset_password_url': build_absolute_uri(f'{reset_password_path}?token={reset_password_token.key}'),
+    }
+
+    notify(ACTIVITY_USER_RESETS_PASS, context=context, email_to=[reset_password_token.user.email])
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    profile_picture = ThumbnailerImageField('ProfilePicture', upload_to='profile_pictures/', blank=True, null=True)
+
+    def get_tokens(self):
+        refresh = RefreshToken.for_user(self)
+
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+
+    def __str__(self):
+        return self.username
+
+
+saved_file.connect(generate_aliases_global)
