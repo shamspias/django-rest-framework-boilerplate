c9191936514b14da82417d5502ac7645c29a48df
shamspias <shamsuddinpias0@gmail.com>
2022-10-16 18:17:23 +0600

    update setting

--- config/settings/stage.py
+++ config/settings/stage.py
@@ -10,7 +10,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'db'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT'),
     }
 }
