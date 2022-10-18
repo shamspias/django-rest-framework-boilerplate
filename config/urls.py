f0c99045bf1bdaffb206ea91322da6ee6b86f826
shamspias <shamsuddinpias0@gmail.com>
2022-10-18 15:48:15 +0600

    update swagger

--- config/urls.py
+++ config/urls.py
@@ -9,12 +9,21 @@ from rest_framework_simplejwt.views import (
 )
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
 
 from users.urls import users_router
 
 schema_view = get_schema_view(
-    openapi.Info(title="BP User API", default_version='v1'),
+    openapi.Info(
+        title="API Title",
+        default_version='v1',
+        description="API for BoilerPlate",
+        terms_of_service="https://github.com/shamspias",
+        contact=openapi.Contact(email="shamsuddinpias0@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
     public=True,
+    permission_classes=[permissions.AllowAny, ],
 )
 
 router = DefaultRouter()
