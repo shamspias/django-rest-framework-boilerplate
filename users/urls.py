7c190a2dc2c95e00286cd1a8f5de088da54b1dc2
shamspias <shamsuddinpias0@gmail.com>
2022-10-19 12:35:31 +0600

    feat: Customer urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import SimpleRouter
 
-from users.views import UserViewSet
+from users.views import UserViewSet, CustomerViewSet
 
 users_router = SimpleRouter()
 
 users_router.register(r'users', UserViewSet)
+users_router.register(r'customers', UserViewSet)
