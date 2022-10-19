23c1cbcc18c3adabfbc44cde9a9af768f49110f4
shamspias <shamsuddinpias0@gmail.com>
2022-10-19 20:41:48 +0600

    refactor: jwt

--- config/settings/common.py
+++ config/settings/common.py
@@ -260,7 +260,7 @@ REST_FRAMEWORK = {
 
 # JWT configuration
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=3),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
